9acf5a0f19c8d0ad03c48d1f5d8a67daecf6a92a
Damilola Oyeyemi <oyeyemidamilola21@gmail.com>
2021-11-24 14:40:47 +0100

    Feat: Created basket repository

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,33 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+	public class BasketRepository : IBasketRepository
+	{
+		private readonly IDistributedCache _cache;
+
+		public BasketRepository(IDistributedCache cache)
+		{
+			_cache = cache;
+		}
+		public async Task<Cart> GetBasket(string userName)
+		{
+			var basket = await _cache.GetStringAsync(userName);
+			return !string.IsNullOrEmpty(basket) ? JsonConvert.DeserializeObject<Cart>(basket) : null; 
+		}
+
+		public async Task<Cart> UpdateBasket(Cart cart)
+		{
+			await _cache.SetStringAsync(cart.UserName, JsonConvert.SerializeObject(cart));
+			return await GetBasket(cart.UserName);
+		}
+
+		public async Task DeleteBasket(string userName)
+		{
+			await _cache.RemoveAsync(userName);
+		}
+	}
+}
---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Basket.API.Entities;
+
+namespace Basket.API.Repositories
+{
+	public interface IBasketRepository
+	{
+		Task<Cart> GetBasket(string userName);
+		Task<Cart> UpdateBasket(Cart cart);
+		Task DeleteBasket(string userName);
+	}
+}
