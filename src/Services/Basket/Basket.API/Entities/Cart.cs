a56b8780629c1b16d811c94a79a5e0d7764254fd
Damilola Oyeyemi <oyeyemidamilola21@gmail.com>
2021-11-24 14:07:56 +0100

    Feat: Added basket entities

---
+++ src/Services/Basket/Basket.API/Entities/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Basket.API.Entities
+{
+	public class Cart
+	{
+		public Cart() { }
+
+		public Cart(string userName)
+		{
+			UserName = userName;
+		}
+		public string  UserName { get; set; }
+		public List<CartItem> Items { get; set; } = new List<CartItem>();
+
+		public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+
+	}
+}
