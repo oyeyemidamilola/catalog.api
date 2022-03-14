2d2990f37593feae95a9a397d21e8e8bee9893a9
Damilola Oyeyemi <oyeyemidamilola21@gmail.com>
2022-03-14 13:45:47 +0100

    Feat: Added Application Layer for Ordering

---
+++ src/Services/Ordering/Ordering.Application/Features/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace Ordering.Application.Features.Queries.GetOrdersList
+{
+	public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+	{
+		public string UserName { get; set; }
+	}
+}
